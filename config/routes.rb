0c6250da505802d44848f61f4340faa418906151
Laurent Barbezieux <laurent.barbezieux@gmail.com>
2020-02-17 17:45:59 +0100

    seed done with 4 models

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :plants
   resources :users
+  resources :bookings
+  resources :reviews
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,13 @@ p "generate new seed"
 julien = User.new(username: "julien-du-93",
                   firstname: "julien",
                   lastname: "lalou")
-julien.save
+julien.save!
+
+antoine = User.new(username: "antoine-du-93",
+                  firstname: "antoine",
+                  lastname: "lalou")
+antoine.save!
+
 
 begonia = Plant.new(name: "beau begonia",
                     price: 10,
@@ -23,3 +29,39 @@ begonia = Plant.new(name: "beau begonia",
                     species: "maman")
 begonia.user = julien
 begonia.save!
+
+tulipe = Plant.new(name: "beau tulipe",
+                    price: 10,
+                    disponibility: true,
+                    species: "maman")
+tulipe.user = antoine
+tulipe.save!
+
+rose = Plant.new(name: "beau rose",
+                    price: 10,
+                    disponibility: true,
+                    species: "maman")
+rose.user = antoine
+rose.save!
+
+booking1 = Booking.new(start_date: DateTime.now,
+                    end_date: DateTime.now,
+                    total_price: 5)
+booking1.user = antoine
+booking1.plant = begonia
+booking1.save!
+
+booking2 = Booking.new(start_date: DateTime.now,
+                    end_date: DateTime.now,
+                    total_price: 10)
+booking2.user = julien
+booking2.plant = tulipe
+booking2.save!
+
+review1 = Review.new(rating: 5, comments: "good")
+review1.booking = booking1
+review1.save!
+
+review2 = Review.new(rating: 1, comments: "bad")
+review2.booking = booking2
+review2.save!
