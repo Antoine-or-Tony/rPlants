b5ad411d752d64f772a13e7340a1a3777fbf1bce
julien laloum <djulesdev@gmail.com>
2020-02-19 16:56:30 +0100

    fixe map probleme

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,8 +24,6 @@ require("channels")
 
 import "bootstrap";
 import { initMapbox } from '../plugins/init_mapbox';
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
-
 document.addEventListener('turbolinks:load', () => {
   initMapbox();
 })
